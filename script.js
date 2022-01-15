05060571cbbf291c807a2f37594652cba4022a68
Ivan Jankov <ivanjankov07@gmail.com>
2022-01-15 17:58:17 +0100

    Brand Filter Implemented

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let minVal, maxVal;
-
 $(function () {
+	let fullProductList = ['lg', 'sony', 'samsung'];
 	let productList = $('.single-product');
-	let selectedItems = [];
 
-	$('#slider-range').slider({
+	let selectedProducts = [];
+	let slideOptions = {
 		range: true,
 		min: 300,
 		max: 1500,
 		values: [300, 1500],
 		slide: function (event, ui) {
 			$('#amount').val('€' + ui.values[0] + ' - €' + ui.values[1]);
-			minVal = ui.values[0];
-			maxVal = ui.values[1];
-			filterProductsByPrice(minVal, maxVal);
 		},
 		change: function (event, ui) {
-			console.log(ui.values[0]);
+			let minVal = ui.values[0];
+			let maxVal = ui.values[1];
+			let filteredProducts = getSelectedItems();
+			updateList(minVal, maxVal, filteredProducts);
+			// filterProductsByPrice(minVal, maxVal);
 		},
-	});
+	};
+
+	let slider = $('#slider-range').slider(slideOptions);
+	let masterCheck = $('#all');
+	let listCheckedItems = $('.checkbox-init');
 
 	$('#amount').val(
 		'€' +
@@ -26,8 +30,75 @@ $(function () {
 			' - €' +
 			$('#slider-range').slider('values', 1)
 	);
-	let price = $('#slider-range').slider('values', 0);
-	console.log(price);
+
+	$('.checkbox-init').on('change', function () {
+		let minPrice = $('#slider-range').slider('values', 0);
+		let maxPrice = $('#slider-range').slider('values', 1);
+		if (this.value == 'all') {
+			selectedProducts = fullProductList;
+		} else if (this.checked) {
+			selectedProducts.push(this.value);
+		} else {
+			selectedProducts.splice(selectedProducts.indexOf(this.value), 1);
+		}
+		let filteredList = getSelectedItems();
+		updateList(minPrice, maxPrice, filteredList);
+		// console.log(selectedProducts);
+	});
+	function updateList(min, max, selected) {
+		productList
+			.hide()
+			.filter((el, ui) => {
+				let data = ui.dataset;
+
+				return (
+					Number(data.price) > min &&
+					Number(data.price) < max &&
+					selected.includes(data.brand)
+				);
+			})
+			.show();
+	}
+
+	// CHECKBOX LOGIC
+
+	masterCheck.change(() => {
+		console.log;
+		let isCheboxAllChecked = $('#all').is(':checked');
+		listCheckedItems.prop('checked', isCheboxAllChecked);
+		getSelectedItems();
+	});
+
+	listCheckedItems.on('change', () => {
+		let totalItems = listCheckedItems.length;
+		let checkedItems = listCheckedItems.filter(':checked').length;
+
+		if (totalItems == checkedItems) {
+			masterCheck.prop('indeterminate', false);
+			masterCheck.prop('checked', true);
+		} else if (checkedItems > 0 && checkedItems < totalItems) {
+			// masterCheck.prop('indeterminate', true);
+			masterCheck.prop('checked', false);
+		} else {
+			masterCheck.prop('indeterminate', true);
+			masterCheck.prop('checked', true);
+		}
+		getSelectedItems();
+	});
+
+	function getSelectedItems() {
+		let getCheckedValues = [];
+		listCheckedItems.filter(':checked').each(function () {
+			getCheckedValues.push($(this).val());
+		});
+
+		if (getCheckedValues.length == 0) {
+			getCheckedValues = fullProductList;
+			masterCheck.prop('checked', true);
+		}
+		console.log(getCheckedValues);
+		return getCheckedValues;
+	}
 });
 
 function filterProductsByPrice(minVal, maxVal) {
